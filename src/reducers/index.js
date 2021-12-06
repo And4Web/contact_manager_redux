099986e21274e9d19e1eb402e74b653a0f638811
Anand Shukla <anands2807@gmail.com>
2021-12-06 15:01:42 +0530

    reducers working.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,7 @@
 import {combineReducers} from 'redux';
 import contactReducer from './contactReducer';
 
-const rootReducer = combineReducers({
+export default combineReducers({
   contact: contactReducer
 });
 
-export default rootReducer;
