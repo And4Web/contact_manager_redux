f8416d4844d6cf07b968a7ba4135837850a138b0
Anand Shukla <anands2807@gmail.com>
2021-12-12 15:11:09 +0530

    addContact functionality added.

--- src/actions/contactActions.js
+++ src/actions/contactActions.js
@@ -18,9 +18,12 @@ export const deleteContact = (id) => async dispatch => {
   });
 }
 
-export const addContact = (contact) => {
-  return {
-    type: ADD_CONTACT,
-    payload: contact
-  }
+export const addContact = (contact) => async dispatch => {
+  const res = await axios.post("https://jsonplaceholder.typicode.com/users", contact)
+  dispatch(
+    {
+      type: ADD_CONTACT,
+      payload: res.data
+    }
+  ) 
 }
